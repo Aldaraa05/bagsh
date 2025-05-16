cbdcb1f771d288a2909c273a25296104d88f3a19
Munkh-Aldar Otgontur <aldaraaotgontur@gmail.com>
2025-05-16 11:45:35 +0800

    1

--- my-appp/app/Infos/page.js
+++ my-appp/app/Infos/page.js
@@ -9,6 +9,7 @@ export default function Infos() {
             <p className="blueText">Тогтвортой хөгжил</p>
             <h1>
               Юнител группийн “Тогтвортой байдлын тайлан 2024”-өөс онцлох ажлууд
+              Юнител группийн “Тогтвортой байдлын тайлан 2024”-өөс онцлох ажлууд
             </h1>
           </div>
           <img src="https://unread.today/files/969a1aed-77a4-45e9-b7b7-3ee2709aaf31/c88200a93f3a73f06149e88537ddaa00_square.jpg" />
